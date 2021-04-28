2edba5139dc639a028015aedf3f7abb7c573a741
Megaoski <oscar_2596@hotmail.com>
2021-04-28 13:05:36 +0200

    Added scene with character movement and camera

---
+++ Into The Woods/Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+
+    private Vector3 CameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = PlayerTransform.position + CameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+}
