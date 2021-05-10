2bb09b12acaf2ad6a52c2f38eb82f700c03c6037
Pol Bosch <pol.hcsob@gmail.com>
2021-05-10 21:50:29 +0200

    Player camera and basic movement done

---
+++ Into The Woods/Assets/Animation/EvilController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(CharacterController))]
+public class EvilController : MonoBehaviour
+{
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        // Rotate around y - axis
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        // Move forward / backward
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        if (curSpeed > 0)
+        {
+            animator.SetInteger("walk", 1);
+        }
+        else
+        {
+            animator.SetInteger("walk", 0);
+        }
+        if (curSpeed >= 0)
+        {
+            controller.SimpleMove(forward * curSpeed);
+        }
+    }
+}
+
