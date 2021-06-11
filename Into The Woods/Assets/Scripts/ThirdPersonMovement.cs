73851d39c95b53ab6715f41851d751379cae0d2a
Clara <rustiktie@gmail.com>
2021-06-11 15:15:36 +0200

    better movement

--- Into The Woods/Assets/Scripts/ThirdPersonMovement.cs
+++ Into The Woods/Assets/Scripts/ThirdPersonMovement.cs
@@ -7,15 +7,18 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
     public Animator animator;
+
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-    public float gravity = -9.81f;
+
+    public float gravity = -19f;
     Vector3 velocity;
-    public float jumpHeight = 1.5f;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+
+    public float jumpHeight = 1.5f;
     bool isGrounded;
     
     // Start is called before the first frame update
@@ -29,38 +32,41 @@ public class ThirdPersonMovement : MonoBehaviour
     {
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        
+
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-        
+
+        //Getting movement input 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        //Move
         if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        {           
+            //Find & set rotation
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
+            //Ask to move
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
             
         }
 
-        //Jump
-        // if (Input.GetButtonDown("Jump") && isGrounded)
-        // {
-        //     velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        // }
-
+       //Jump
+         if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
+        //Gravity
         velocity.y += gravity * Time.deltaTime;
-
         controller.Move(velocity * Time.deltaTime);
 
-        // Animation
+        //Animation
         float velocityZ = Vector3.Dot(direction.normalized, transform.forward);
         float velocityX = Vector3.Dot(direction.normalized, transform.right);
 
